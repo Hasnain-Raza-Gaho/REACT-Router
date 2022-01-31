016c1dd46b1119366af64d77c031133d3dd0379d
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-01-31 19:17:59 +0500

    useLocation and UseNavigation example

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const [name,setName] = useState(null);
+    const [course,setCourse] = useState(null);
+    const [classs,setClass] = useState(null);
     return (
         <div>
 <p>this is from home page</p>
+
+Name: <input type="text" onChange={(e)=> setName(e.target.value)} /> <br />
+Class: <input type="text" onChange={(e)=> setClass(e.target.value)} /> <br />
+Course: <input type="text" onChange={(e)=> setCourse(e.target.value)} />
+
+
+<br />
+<button onClick={()=>navigate('./about', {state:{
+    name,
+    classs,
+    course
+}})}>Sign In</button>
         </div>
     );
 }
--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {useLocation} from 'react-router-dom';
 
 const About = () => {
+    const loc = useLocation();
+    console.log(loc)
     return (
         <div>
-            <p>This is from about page</p>
+            <h1>My Name is {loc.state.name}</h1>
+            <p>I read in class {loc.state.classs}</p>
+            <p>I am Learning {loc.state.course} in Saylani</p>
         </div>
     );
 }
