67e56cd27fb85de0eaeb40f6be723eaa797d40b6
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-02-05 09:02:51 +0500

    public and private routes starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import {  Routes, Route, Navigate} from 'react-router-dom';
-import About from './components/about'
-import Home from './components/Home'
-
 
 const App = () => {
- 
-
-
   return (
     <div>
-      App Page
-      <Routes>
-    <Route path='/about' element={<About/>} />
-    <Route path='/' element={<Home/>} />
-    <Route path='*' element={<Navigate to='/about'/>} />
-    
-      </Routes>
-
-
-      
+      <h1>Public and Private Routes</h1>
     </div>
   );
 }
