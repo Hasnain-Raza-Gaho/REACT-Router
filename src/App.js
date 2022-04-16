dc8ae9fd1adf92c94df7aa94234477c880fa6a4a
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-04-16 10:05:05 +0500

    done work today

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const App = () => {
+  const date = new Date();
+  console.log(date.toJSON())
   return (
     <div>
       <h1>Public and Private Routes</h1>
+      <h2>data</h2>
+      <p>{date.toString().slice(4,24)}</p>
     </div>
   );
 }
